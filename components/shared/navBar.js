b61fc5b02722cd7b9b2d289a6a9e9ff0bf8272c5
Daun Kim <yennakim11@gmail.com>
2023-12-06 17:43:36 -0600

    navbar appears along with functioning logout button

---
+++ components/shared/navBar.js
@@ -0,0 +1,30 @@
+import renderToDom from '../../utils/renderToDom';
+
+const navBar = () => {
+  const domString = `
+  <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary mb-5">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">vocab-YOU-lary</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link " href="#" id="all-words">All Words <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Other</a>
+        </li>
+      </ul>
+      <span class="navbar-text">
+      <div id="logout-button"></div>
+    </span>
+    </div>
+  </div>
+</nav>`;
+
+  renderToDom('#navigation', domString);
+};
+
+export default navBar;
