3a18a58af26fb1819aff9246638479fd44512967
Daun Kim <yennakim11@gmail.com>
2023-12-06 22:40:24 -0600

    created promises for word CRUD and updated info in package.json

--- api/wordData.js
+++ api/wordData.js
@@ -15,4 +15,47 @@ const getWords = () => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
-export default getWords;
+// CREATE WORD
+const createWord = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/words.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application.json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// UPDATE WORD
+const updateWord = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/words/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application.json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// DELETE WORD
+const deleteWord = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/words/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getWords, createWord, updateWord, deleteWord
+};
