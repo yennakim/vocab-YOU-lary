487df0609a71a2a98d2c117c6382acb739424cfb
Daun Kim <yennakim11@gmail.com>
2023-12-08 20:02:07 -0600

    fixed flexbox to show horizontally and fixed welcome h1 to disappear after sign in

--- pages/words.js
+++ pages/words.js
@@ -17,8 +17,8 @@ const showWords = (array) => {
      <h5 class="card-title">${item.title}</h5>
      <h6 class="card-subtitle mb-2 text-body-secondary">Category: ${item.category}</h6>
      <p class="card-text">${item.definition}</p>
-     <i id="edit-word-btn--${item.firebaseKey}" class="fas fa-edit btn btn-info"></i>
-     <i id="delete-word-btn--${item.firebaseKey}" class="btn btn-danger fas fa-trash-alt"></i>
+     <a href='#' id="edit-word-btn--${item.firebaseKey}">Edit</a>
+     <a href='#' id="delete-word-btn--${item.firebaseKey}">Delete</a>
    </div>
  </div>`;
   });
